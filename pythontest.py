485fb356ca604dbfcbb1eea8463b462bfc09df62
bob.wang <bobw@sina.com>
2020-05-17 19:49:25 +0800

    the 2nd commit

--- pythontest.py
+++ pythontest.py
@@ -2,9 +2,6 @@ import pymongo
 from base64 import b64encode
 
 print('运行请按ctrl-shift-F10.')
-print('welcome to qytang..')
-# print('welcome to qytang')
-# print('welcome to qytang')
-# print('welcome to qytang')
-# # print('welcome to qytang')
-# print('welcome to qytang')
+print('welcome to github..')
+print('Hello world')
+
