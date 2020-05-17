ec79347f136904eece115cacb05fb64caf117265
bob.wang <bobw@sina.com>
2020-05-17 19:29:06 +0800

    Initial commit

---
+++ pythontest.py
@@ -0,0 +1,10 @@
+import pymongo
+from base64 import b64encode
+
+print('运行请按ctrl-shift-F10.')
+print('welcome to qytang..')
+# print('welcome to qytang')
+# print('welcome to qytang')
+# print('welcome to qytang')
+# # print('welcome to qytang')
+# print('welcome to qytang')
